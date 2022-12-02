031e20e9aa8fbdecea5d386c74c6e5d352801308
Rakesh <114543952+rakesh-th@users.noreply.github.com>
2022-12-02 00:34:42 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -11,7 +12,7 @@ from xgboost import XGBClassifier
 
 st.header("Credit Card Approval Prediction")
 st.text_input("Enter your Name: ", key="name")
-my_data = pd.read_csv("studentscores.csv")
+my_data = pd.read_csv("final_application.csv")
 
 # load model
 #best_xgboost_model = XGBClassifier()
@@ -19,23 +20,79 @@ my_data = pd.read_csv("studentscores.csv")
 
 if st.checkbox('Show Training Dataframe'):
     my_data
+
+st.subheader("Please provide details of your application!")
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Gender = st.radio(
+        'Gender of the applicant:',
+        np.unique(my_data['Gender']))
  
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Car = st.radio(
+        'Does the applicant own a car:',
+        np.unique(my_data['Car']))
 
-input_hours = st.slider('Number of Hours:', 0.0, max(my_data["Hours"]), 2.5)
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Realty = st.radio(
+        'Does the applicant own any Realty(properties):',
+        np.unique(my_data['Realty']))
 
-X = my_data.iloc[ : ,   : 1 ].values
-Y = my_data.iloc[ : , 1 ].values
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Family_Status = st.radio(
+        'Is applicant Single/Married:',
+        np.unique(my_data['Family_Status']))
 
-X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size = 1/4, random_state = 0) 
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Income_Type = st.radio(
+        "What is the applicant's source of Income:",
+        np.unique(my_data['Income_Type']))   
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor = regressor.fit(X_train, Y_train)
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_House_Type = st.radio(
+        "What is the applicant'type of House",
+        np.unique(my_data['House_Type']))
 
-#np.expand_dims([int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
+left_column, right_column = st.columns(2)
+with left_column:
+    inp_Education = st.radio(
+        'Education of the Applicant',
+        np.unique(my_data['Education']))  
+
+input_Children = st.slider('Number of Children:', 0, max(my_data["Children"]), 3)
+input_Family_Size = st.slider('Family Size:', 0, max(my_data["Family_Size"]), 4)
+input_Salary = st.slider('Salary of the Applicant:', 0.0, max(my_data["Salary"]), 85000.0)
+input_AGE = st.slider('Age of the applicant in Years:', 0.0, max(my_data["AGE"]), 22.8)
+input_EXPERIENCE = st.slider('Experience of the applicant in Years:', 0.0, max(my_data["EXPERIENCE"]), 7.5)
+input_ACCOUNT_DURATION = st.slider('Account Duration with the bank in Months:', 0, max(my_data["ACCOUNT_DURATION"]), 18)
+
+
+X = my_data.drop(['ID', 'Risk', 'Occupation_Type'], axis=1)
+y = my_data['Risk']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 123)
+
+#adasyn = ADASYN()
+#X_train,y_train = adasyn.fit_resample(X_train,y_train)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+best_xgboost_model = XGBClassifier(max_depth=5,n_estimators=250, min_child_weight=8)
+best_xgboost_model.fit(X_train, y_train)
+
 if st.button('Make Prediction'):
-    inputs = np.expand_dims([input_hours],0)
-    prediction = regressor.predict(inputs)
-    st.write(f"SCore: {prediction}")
+    inputs = np.expand_dims([inp_Gender, inp_Car, inp_Realty, input_Children, input_Salary, inp_Income_Type, inp_Education, inp_Family_Status, inp_House_Type, input_AGE, input_EXPERIENCE, input_Family_Size, input_ACCOUNT_DURATION],0)
+    prediction = best_xgboost_model.predict(inputs)
+    if prediction:
+        st.write("Your Credit Card is Declined")
+    else:
+        st.write("Your Credit Card is Approved")
 
     st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
